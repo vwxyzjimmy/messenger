10e9793cb24e5417b0fa539a42c2319d3f8f6f8a
vwxyzjimmy <vwxyzjimmy@gmail.com>
2018-05-01 08:38:57 +0000

    change varify taken

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ app.post('/webhook', (req, res) => {
 app.get('/webhook', (req, res) => {
   
   /** UPDATE YOUR VERIFY TOKEN **/
-  const VERIFY_TOKEN = "<YOUR VERIFY TOKEN>";
+  const VERIFY_TOKEN = "EAAeWmZBYCMeABAOceYcbUUZAiFZC6XX8AM6t9ZC9h1vfiwlLSVPRhygHzJzrchFdg6Yi1hzJnS8puZAGzNMiHnV6ebkZCrBZCrEZBUmq5u3oUdp5IuxqZBKc9mTm2ZCg9EQt3ZBiFFvouEqZA1EanW5ZAATF83pKDmN0tP73OZBsW32CKs0gZDZD";
   
   // Parse params from the webhook verification request
   let mode = req.query['hub.mode'];
